2d22625d9ab2ad91cdc485d6e0d61a76cf48bd20
Maria Carneiro <dudinhatc02@gmail.com>
2025-01-12 12:55:33 -0800

    creating dir for frontend

---
+++ frontend/src/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import './LoadingPage.css'
+import planeLoading from '../../assets/planeLoading.gif'
+
+export default function LoadingPage() {
+    return (
+        <div id="loading">
+            <img src={planeLoading} alt="" />
+            <h4>Wait for a moment, we are loading your request</h4>
+        </div>
+    )
+}
