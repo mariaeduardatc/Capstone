78627c7512c72e468a5d58aff69901ae0a25efc7
Maria Carneiro <dudinhatc02@gmail.com>
2025-01-12 13:14:46 -0800

    test git

--- frontend/src/components/RegisterPage/RegisterPage.tsx
+++ frontend/src/components/RegisterPage/RegisterPage.tsx
@@ -69,7 +69,7 @@ export default function RegisterPage() {
       }
 
       const res = await postAPICall(responseInput);
-
+      // test
       if (res?.status === 200) {
         setIsLoading(false);
         setIsAuthenticated(res.body);
