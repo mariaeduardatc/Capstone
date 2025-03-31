2c6180bf1512a591eb98b75ef11c4a13c5b9e1d0
Maria Eduarda Távora Carneiro <mariacarneiro@uni.minerva.edu>
2025-03-30 18:05:27 -0700

    small change https

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://capstone-six-mocha.vercel.app',
     methods: 'GET,POST',
     credentials: true
 }));
