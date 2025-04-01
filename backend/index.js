15350ee87ba1095b04b746306b89f7b247542aa4
Maria Eduarda Távora Carneiro <mariacarneiro@uni.minerva.edu>
2025-03-31 23:02:27 -0700

    Deploy (#47)
    * changing endpoint
    
    * test deploy sql script
    
    * test deploy sql script
    
    * change deploy packahe.json postinstall
    
    * adding port env
    
    * Fix Railway deployment issue
    
    * Fix Railway deployment issue
    
    * Fix Railway deployment issue
    
    * adding places script
    
    * test port
    
    * testing postbuild
    
    * testing start
    
    * testing start
    
    * test postbuild w port
    
    * reverting changes
    
    * testing endpoint change
    
    * testing port

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const ItineraryRouter = require('./app/routers/ItineraryRouters')
 const authenticationMiddleware = require('./app/middlewares/authMiddleware')
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,19 +25,6 @@ passport.use('jwt', authenticationMiddleware.jwtStrategy);
 app.use('/userpage', authenticationMiddleware.authenticateRequest, (req, res) => {
   res.json({ message: 'You have access to the protected route' });
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Server is running');
-// });
-
-// app.get('/health', (req, res) => {
-//   res.status(200).send('OK');
-// });
-
-// process.on('SIGTERM', () => {
-//   console.log('Process terminated');
-//   process.exit(0);
-// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
